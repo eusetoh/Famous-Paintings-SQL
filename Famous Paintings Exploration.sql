27c3729903aaa629e754ef60b1db991cdca079a8
eusetoh <156874929+eusetoh@users.noreply.github.com>
2024-12-04 13:15:53 +0800

    Add files via upload

---
+++ Famous Paintings Exploration.sql
@@ -0,0 +1,31 @@
+/*select the schema*/
+USE famous_paintings;
+
+/*find the average price of an artwork by artist*/
+CREATE TEMPORARY TABLE temp_table AS 
+	SELECT product_size.sale_price, product_size.work_id, work.artist_id, artist.full_name
+    FROM product_size
+    LEFT JOIN work 
+		ON work.work_id = product_size.work_id
+	LEFT JOIN artist
+		ON artist.artist_id = work.artist_id;
+    
+/*list all the museums which are open on both sunday and monday*/
+CREATE TEMPORARY TABLE monday_museum AS 
+	SELECT name, day
+		FROM museum_hours
+		INNER JOIN museum
+			ON museum.museum_id = museum_hours.museum_id
+		WHERE day = "Monday";
+        
+CREATE TEMPORARY TABLE sunday_museum AS 
+	SELECT name, day
+		FROM museum_hours
+		INNER JOIN museum
+			ON museum.museum_id = museum_hours.museum_id
+		WHERE day = "Sunday";
+        
+SELECT monday_museum.name
+	FROM monday_museum
+    LEFT JOIN sunday_museum
+		ON sunday_museum.name = monday_museum.name;
